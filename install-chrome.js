230cb62604fc329c1730b67a2567699e62974c9d
crimsondotns <cswsye147@gmail.com>
2025-07-21 18:06:48 +0700

    Delete install-chrome.js

--- install-chrome.js
+++
@@ -1,5 +0,0 @@
-const puppeteer = require('puppeteer');
-puppeteer
-  .install()
-  .then(() => console.log('✅ Chromium installed successfully'))
-  .catch(err => console.error('❌ Failed to install Chromium:', err));
